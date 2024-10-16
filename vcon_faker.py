dff1cc23d3634c533bb7f3eb159aa7e1e9ab07a6
Thomas Howe <ghostofbasho@gmail.com>
2024-10-16 14:55:51 -0400

    Updates vcon library

--- vcon_faker.py
+++ vcon_faker.py
@@ -11,9 +11,17 @@ from vcon.dialog import Dialog
 
 from datetime import datetime
 import uuid
-from fake_names import male_names, female_names, last_names, businesses, problems, emotions
+from fake_names import (
+    male_names,
+    female_names,
+    last_names,
+    businesses,
+    problems,
+    emotions,
+)
 import warnings
-warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+
+warnings.filterwarnings(action="ignore", category=DeprecationWarning)
 from openai import OpenAI
 
 load_dotenv()
@@ -28,21 +36,28 @@ OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 OPENAI_MODEL = st.secrets["OPENAI_MODEL"]
 OPENAI_TTS_MODEL = st.secrets["OPENAI_TTS_MODEL"]
 
-print(f"Using model: {OPENAI_MODEL}, TTS model: {OPENAI_TTS_MODEL} and S3 bucket: {S3_BUCKET}")
+print(
+    f"Using model: {OPENAI_MODEL}, TTS model: {OPENAI_TTS_MODEL} and S3 bucket: {S3_BUCKET}"
+)
 
 client = OpenAI()
 client.api_key = OPENAI_API_KEY
 
 
-def generate_conversation(prompt, agent_name, customer_name, business, problem, emotion, business_name):
+def generate_conversation(
+    prompt, agent_name, customer_name, business, problem, emotion, business_name
+):
     completion = client.chat.completions.create(
         model=OPENAI_MODEL,
-        response_format={ "type": "json_object" },
+        response_format={"type": "json_object"},
         messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-            {"role": "user", "content": prompt}
-        ]
-        )
+            {
+                "role": "system",
+                "content": "You are a helpful assistant designed to output JSON.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+    )
     result = json.loads(completion.choices[0].message.content)
     return result.get("conversation", [])
 
@@ -80,17 +95,21 @@ col1, col2 = st.columns(2)
 business = col2.selectbox("Select Business", businesses)
 problem = col2.selectbox("Select Problem", problems)
 business_name = col2.text_input("Business Name", "a random business")
-col1.markdown("This app generates fake conversations between a customer and \
+col1.markdown(
+    "This app generates fake conversations between a customer and \
             an agent. The conversation is generated based on a prompt and \
             includes the names of the agent and customer, the business, \
             the problem, and the emotion of the customer.  The conversation \
             is then synthesized into an audio file, a vCon is created then \
-            it is uploaded into S3.") 
+            it is uploaded into S3."
+)
 
 add_emotion = col2.checkbox("Add emotion to conversation.")
 num_conversations = col2.number_input("Number of Conversations to Generate", 1, 100, 1)
 generate = col2.button("Generate Conversation(s)")
-st.toast(f"Configured to use model: {OPENAI_MODEL}, TTS model: {OPENAI_TTS_MODEL} and S3 bucket: {S3_BUCKET}")
+st.toast(
+    f"Configured to use model: {OPENAI_MODEL}, TTS model: {OPENAI_TTS_MODEL} and S3 bucket: {S3_BUCKET}"
+)
 
 # Display the instructions in the sidebar
 with st.sidebar:
@@ -113,7 +132,9 @@ with st.sidebar:
 
     """
     st.markdown(instructions)
-    conversation_prompt = st.text_area("Conversation Prompt (Editable)", default_conversation_prompt, height=400)
+    conversation_prompt = st.text_area(
+        "Conversation Prompt (Editable)", default_conversation_prompt, height=400
+    )
 
 if generate:
     completed_conversations = []
@@ -137,16 +158,18 @@ if generate:
         # Create a new vcon UUID
         vcon_uuid = str(uuid.uuid4())
 
-
-        total_bar.progress((i+1)/num_conversations, text=f"Generating conversation {i+1} of {num_conversations}")
+        total_bar.progress(
+            (i + 1) / num_conversations,
+            text=f"Generating conversation {i+1} of {num_conversations}",
+        )
         this_bar = st.progress(0, text="Generating conversation transcript.")
-        
-        while business == 'Pick Random Business Type':
+
+        while business == "Pick Random Business Type":
             business = random.choice(businesses)
-            
-        while problem == 'random situation':
+
+        while problem == "random situation":
             problem = random.choice(problems)
-        
+
         generation_prompt = conversation_prompt
         generation_prompt += f"\n\nIn this conversation, the agent's name is {agent_name} and the customer's name is {customer_name}.  "
         generation_prompt += f"The conversation is about {business_name} (a {business} ) and is a conversation about {problem}."
@@ -160,22 +183,20 @@ if generate:
             business=business,
             problem=problem,
             emotion=emotion,
-            business_name=business_name)
-        
+            business_name=business_name,
+        )
+
         this_bar.progress(0.2, text="Synthesizing conversation audio")
         # Process each line of the conversation
         audio_files = []  # Initialize an empty list to store audio files
-        voices = ["alloy", "echo", "fable", "onyx", "nova",  "shimmer"]
-        
+        voices = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
+
         # Pick a different random voice for each speaker
         agent_voice = random.choice(voices)
         # Remove the agent voice from the list so we don't pick it again
         voices.remove(agent_voice)
         customer_voice = random.choice(voices)
-        voices = {
-            "Agent": agent_voice,
-            "Customer": customer_voice
-        }
+        voices = {"Agent": agent_voice, "Customer": customer_voice}
 
         combined_audio = AudioSegment.silent(duration=0)
         for item in generated_conversation:
@@ -183,17 +204,17 @@ if generate:
             # Make sure it's a dict, and that it has a 'message' key
             if not isinstance(item, dict):
                 continue
-            if 'message' not in item:
+            if "message" not in item:
                 continue
-            if len(item['message']) == 0:
+            if len(item["message"]) == 0:
                 continue
-            role = item['speaker']
+            role = item["speaker"]
             speech_file_path = "_temp.mp3"
             response = client.audio.speech.create(
-                input=item['message'],
+                input=item["message"],
                 model=OPENAI_TTS_MODEL,
                 response_format="mp3",
-                voice=voices[role]
+                voice=voices[role],
             )
             response.stream_to_file(speech_file_path)
             # Append the audio file to the list
@@ -212,7 +233,9 @@ if generate:
         audio_duration = len(combined_audio) / 1000
 
         # Upload the audio file to S3 bucket
-        s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+        s3_client = boto3.client(
+            "s3", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY
+        )
         bucket_name = S3_BUCKET
 
         # Create the bucket if it doesn't exist
@@ -239,20 +262,17 @@ if generate:
         url = f"https://{bucket_name}.s3.amazonaws.com/{s3_path}"
 
         # Now create the vCon from this conversation
-                # Create a vCon object from the generated conversation
+        # Create a vCon object from the generated conversation
 
         agent_party = Party(
-            name=agent_name,
-            tel=agent_phone,
-            mailto=agent_email,
-            meta={"role": "agent"}
+            name=agent_name, tel=agent_phone, mailto=agent_email, meta={"role": "agent"}
         )
 
         customer_party = Party(
             name=customer_name,
             tel=customer_phone,
             mailto=customer_email,
-            meta={"role": "customer"}
+            meta={"role": "customer"},
         )
 
         # Need to add direction to the conversationFIX
@@ -262,13 +282,13 @@ if generate:
             type="recording",
             body="",
             disposition="ANSWERED",
-            duration=audio_duration
+            duration=audio_duration,
         )
         dialog_info.add_external_data(url, file_name, "audio/mp3")
 
         generation_info = {
-             "type": "generation_info",
-             "body": {
+            "type": "generation_info",
+            "body": {
                 "agent_name": agent_name,
                 "customer_name": customer_name,
                 "business": business,
@@ -276,34 +296,34 @@ if generate:
                 "emotion": emotion,
                 "prompt": generation_prompt,
                 "created_on": datetime.now().isoformat(),
-                "model": OPENAI_MODEL                 
-             }
+                "model": OPENAI_MODEL,
+            },
         }
 
         analysis_info = {
             "dialog": 0,
-            "vendor": 'openai',
+            "vendor": "openai",
             "type": "analysis_info",
             "body": generated_conversation,
             "extra": {
-                "vendor_schema": {
-                    "model": OPENAI_MODEL,
-                    "prompt": generation_prompt                    
-                }
-            }
+                "vendor_schema": {"model": OPENAI_MODEL, "prompt": generation_prompt}
+            },
         }
 
         vcon_obj = Vcon.build_new()
+        print(vcon_obj.to_json())
         vcon_obj.add_party(agent_party)
         vcon_obj.add_party(customer_party)
         vcon_obj.add_dialog(dialog_info)
-        vcon_obj.add_attachment(**generation_info,)
+        vcon_obj.add_attachment(
+            **generation_info,
+        )
         vcon_obj.add_analysis(**analysis_info)
         vcon_json = vcon_obj.to_json()
         vcon_file = f"{vcon_uuid}.vcon.json"
-        with open(vcon_file, 'w') as file:
+        with open(vcon_file, "w") as file:
             file.write(vcon_json)
-            
+
         # Upload the vCon JSON file to S3 bucket
         s3_path = f"{year}/{month}/{day}/{vcon_file}"
 
@@ -312,18 +332,22 @@ if generate:
         s3_client.upload_file(vcon_file, bucket_name, s3_path)
 
         # Get the URL of the uploaded file
-        vcon_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': s3_path})
+        vcon_url = s3_client.generate_presigned_url(
+            "get_object", Params={"Bucket": bucket_name, "Key": s3_path}
+        )
 
         summary = f"Conversation between {agent_name} and {customer_name} about {business_name}, a {business},  related to {problem}. "
         if add_emotion:
             summary += f"The customer is {emotion}"
-            
-        completed_conversations.append({
-            "vcon_uuid": vcon_uuid,
-            "vcon_url": vcon_url,
-            "creation_time": datetime.now().isoformat(),
-            "summary": summary
-        })           
+
+        completed_conversations.append(
+            {
+                "vcon_uuid": vcon_uuid,
+                "vcon_url": vcon_url,
+                "creation_time": datetime.now().isoformat(),
+                "summary": summary,
+            }
+        )
         # Remove the temporary files
         os.remove(combined_file)
         os.remove(vcon_file)
@@ -337,6 +361,3 @@ if generate:
         st.markdown(conversation["summary"])
         st.markdown(f"**vCon URL:** [Download vCon]({conversation['vcon_url']})")
         st.markdown("---")
-
-
-
